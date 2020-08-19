7be291f44b8cb21bcebc730562ecbc48ec4dff7a
Jared Dantis <jareddantis@gmail.com>
2020-08-20 00:32:48 +0800

    commands: Format status as embed

--- commands.py
+++ commands.py
@@ -2,6 +2,7 @@ import logging
 import os
 from mcstatus import MinecraftServer
 from discord.ext import commands
+from discord import Embed
 
 
 def api_error_handler(err):
@@ -53,23 +54,27 @@ class Diagnostic(Category):
         emoji = ':gear:' if pending is not None else ':white_check_mark:' if is_online else ':x:'
         server_state = str(pending) if pending is not None else 'up' if is_online else 'down'
         response_title = f"{emoji} Server is {server_state}{mc_invite}"
-        response_body = f"```\nMinecraft server: {'' if mc else 'not '}running\n" \
-            f"DigitalOcean VPS: {'up' if server else 'down'}\n" \
-            f"Pending operations: {pending}\n" \
-            f"VPS direct IP: {ip}\n" \
-            f"VPS CloudFlare alias: {domain}\n"
+        response_body = Embed(color=0xffa41c if pending is not None else 0x80ea5b if is_online else 0xff371c)
+        response_body.add_field(name="Friendly URL", value=mc_url, inline=True)
+        response_body.add_field(name="Minecraft server", value='Up' if mc else 'Down', inline=True)
+        response_body.add_field(name="DigitalOcean server", value='Up' if server else 'Down', inline=True)
+        response_body.add_field(name="Alternate URL", value=domain, inline=True)
+        response_body.add_field(name="Direct IP", value=ip, inline=True)
         if mc:
             # Query MC server
             mc_api = MinecraftServer.lookup(domain)
             mc_status = mc_api.status()
             mc_query = mc_api.query()
+            mc_sw_ver = mc_query.software.version
+            mc_sw_dist = mc_query.software.brand
+            mc_players_count = mc_status.players.online
+            mc_players_max = mc_status.players.max
             mc_players = ', '.join(mc_query.players.names)
-            response_body += f"Server version: {mc_query.software.version} ({mc_query.software.brand})\n" \
-                f"Bot-to-server latency: {mc_status.latency} ms\n" \
-                f"Players online ({mc_status.players.online}/{mc_status.players.max}): {mc_players}"
-        response_body += '```'
+            response_body.add_field(name="Bot-to-server ping", value=f"{mc_status.latency} ms", inline=True)
+            response_body.add_field(name="Server version", value=f"{mc_sw_ver} ({mc_sw_dist})")
+            response_body.add_field(name=f"Players online ({mc_players_count}/{mc_players_max})", value=mc_players)
         await ctx.send(response_title)
-        await ctx.send(response_body)
+        await ctx.send(embed=response_body)
 
         logging.info(f"'{ctx.command}' command called by {ctx.author}. Response was {response_body}")
 
